a1a27314bdfd53de2674b6dc8dfccfdffcc700f4
arshad3e <arshad.3e@gmail.com>
2025-03-05 23:50:41 -0500

    Update Unique_ptr.cpp

--- Unique_ptr.cpp
+++ Unique_ptr.cpp
@@ -5,4 +5,18 @@ void uniquePtrExample() {
     std::cout << *ptr << std::endl;
 } // `ptr` is automatically deleted when function ends
 
+#include <memory>
 
+void sharedPtrExample() {
+    std::shared_ptr<int> ptr1 = std::make_shared<int>(10);
+    std::shared_ptr<int> ptr2 = ptr1; // Now both share ownership
+
+    std::cout << "Count: " << ptr1.use_count() << std::endl; // 2
+} // When the last shared_ptr goes out of scope, memory is freed
+
+#include <memory>
+
+void weakPtrExample() {
+    std::shared_ptr<int> shared = std::make_shared<int>(20);
+    std::weak_ptr<int> weak = shared; // Doesn't increase reference count
+} // `shared` deletes memory; `weak` doesn't prevent deletion
