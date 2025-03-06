25d0a7e3e70c1231003c98b07fa2abe4daf8fb69
arshad3e <arshad.3e@gmail.com>
2025-03-05 23:50:00 -0500

    Update Unique_ptr.cpp

--- Unique_ptr.cpp
+++ Unique_ptr.cpp
@@ -1,1 +1,8 @@
+#include <memory> // Required for smart pointers
+
+void uniquePtrExample() {
+    std::unique_ptr<int> ptr = std::make_unique<int>(10); // No need to delete
+    std::cout << *ptr << std::endl;
+} // `ptr` is automatically deleted when function ends
+
 
